df759238c4cea9b446b5580144ff09f5c9321809
kath424 <acostak424@gmail.com>
2024-04-27 07:32:47 +0000

    Project pimera prueba unitaria - 4Geeks

---
+++ app.js
@@ -0,0 +1,36 @@
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+    // Convertimos el valor a yenes
+    let valueInYenes = (valueInDollar / 1.07) * 156.5;
+    // Retornamos el valor en dólares
+    return valueInYenes;
+}
+
+const fromYenToPound = function(valueInYenes) {
+    // Convertimos el valor a  british pound
+    let valueInPound = (valueInYenes / 156.5) * 0.87;
+    // Retornamos el valor en dólares
+    return valueInPound;
+}
+
+// Esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// Solo un registro en consola para nosotros
+console.log(sum(7,3))
+
+// Exporta la función para usarla en otros archivos 
+// (similar a la palabra clave "export" cuando se usa webpack)
+module.exports = { sum };
+
+// Tenemos que incluir la función en el exports para que sea exportada a otros archivos como test.js
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound }
